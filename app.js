7b1ec2e2c07af7919e035c92052427d453a490b7
Christopher Mangin <mangin.christopher@gmail.com>
2019-12-20 14:41:52 -0500

    updates to index, budget operators initialized

---
+++ app.js
@@ -0,0 +1,27 @@
+// Budget Operators
+
+var budgetController = (function() {
+    }
+);
+
+
+
+
+
+var UIController = (function() {
+    }
+);
+
+
+
+
+
+var controller = (function() {
+    }
+)(budgetController, UIController);
+
+
+
+
+
+controller.init();
