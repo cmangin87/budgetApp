dba35b1bed2e14115734b6c38fd1e3b222409ece
Christopher Mangin <mangin.christopher@gmail.com>
2019-12-21 10:16:29 -0500

    index/app updates

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // Budget Operators
 
 var budgetController = (function() {
+
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+        this.percentage = -1;
+    };
+
     }
 );
 
